607e8952190957d1502f331988b880247ce8b14e
Ahmad Alobaid <ahmad88me@gmail.com>
2019-03-20 17:21:52 +0300

    update

--- commons/__init__.py
+++ commons/__init__.py
@@ -6,13 +6,38 @@ proj_path = (os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir
 data_dir = os.path.join(proj_path, 'data')
 meta_dir = os.path.join(proj_path, 'meta')
 
+# kinds
+NOMINAL = "nominal"
+ORDINAL = "ordinal"
+RATIO_INTERVAL = "ratio-interval"
 
-def t2dv2_columns_of_kind(num_kind):
+# sub kinds
+CATEGORICAL = "categorical"
+SEQUENTIAL = "sequential"
+RANDOM = "random"
+COUNTS = "counts"
+OTHER = "other"
+
+
+# I am not sure of the below is useful
+# kinds and subkinds
+KINDS = {
+    ORDINAL: [""],
+    NOMINAL: [CATEGORICAL, SEQUENTIAL, RANDOM],
+    RATIO_INTERVAL: [COUNTS, OTHER],
+}
+
+
+def t2dv2_columns_of_kind(num_kind, sub_kind=None):
     """
     :param num_kind: nominal, ordinal, ratio-interval
     :return: a dataframe of the specified kind
     """
     meta_file_dir = os.path.join(meta_dir, 'T2Dv2_typology.csv')
     df = pd.read_csv(meta_file_dir)
-    dfkind = df[df.kind==num_kind]
+    if sub_kind is None:
+        dfkind = df[df.kind==num_kind]
+    else:
+        dfkind = df[df.kind == num_kind and df.sub_kind == sub_kind]
+    print(dfkind)
     return dfkind
--- label/cmd.py
+++ label/cmd.py
@@ -20,7 +20,11 @@ if __name__ == '__main__':
         test_name = "few args"
 
     if test_name == 't2dv2':
-        commons.t2dv2_columns_of_kind("nominal")
+        # commons.t2dv2_columns_of_kind("nominal")
+        for kind in commons.KINDS:
+            for sub in commons.KINDS[kind]:
+                commons.t2dv2_columns_of_kind(kind, sub)
+
         print("T2Dv2 test is still under construction")
     elif test_name == 'olympic':
         print("Olympic Games test is under construction")
