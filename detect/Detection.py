3088e355f8e794165dea272fbce2a5913089a74a
Emilia Kacprzak <emi@Emilias-MacBook.local>
2019-03-22 18:54:12 +0000

    adding conditions in detection

--- detect/Detection.py
+++ detect/Detection.py
@@ -9,7 +9,7 @@ logger = set_config(logging.getLogger(__name__))
 
 """
 Questions:
-1) where we should add a contidion that numbers cennot be negative or have to be real;
++1) where we should add a contidion that numbers cennot be negative or have to be real;
 we dont check that now; not sure for which cases it is necessary now
 Answer: I added that in the categorical, and it should be added to the sequencial as well
 2) should this be sequential? [0,2,4,6,9,12,15]
@@ -18,15 +18,16 @@ Answer: This looks like a simple counts to me.
 necessary and what is the treshold for that?
 Answer: I think Hierarchical can be labeled if they are from the same source (within the same range and have the
 same number of digits)
-4) in hierarchical do all numbers have to have the same number of digits or like 80%?
+4-7) in hierarchical do all numbers have to have the same number of digits or like 80%?
 Answer: All of them (we assume the data is clean in a way) if not, it should be much higher like 99% and less than 10 items
-5) in sequential how many of values have to have the same difference? currently (int(len(diffs)/2))
++5) in sequential how many of values have to have the same difference? currently (int(len(diffs)/2))
 Answer: I would say the majority, but maybe we need to look at the data to have a more scientific argument
 6) what should be with [1,1,1,1,1,1] or [2,2,2,2,2,2]
-Answer: this should be ignored
-7) hierarchical now has that 80% of values have to have the same length - could be changed
+Answer: this should be ignored;
+E: so what is the condition for categorical? cause it fits our current condition
+7-4) hierarchical now has that 80% of values have to have the same length - could be changed
 Answer: let us make it a much, and have a table in the paper about the pros and cons for each.
-8) i dont fully understand when something with positive and real values would not get to ratiointerval case -
++8) i dont fully understand when something with positive and real values would not get to ratiointerval case -
 is that okay that in all cases it will fall there? there is a really likaly there will be a lot of random crap there
 Answer: Yes, ratio-interval-other would have a lot of crap as a result. Because we are not able to detect random nominals
 9) should this be [1,1,3,3,3,3,3,5] categorical or ratiointerval?
@@ -75,7 +76,7 @@ class Detection(object):
 
     def is_ordinal(self):
         diffs = [j-i for i, j in zip(self.cleanValues[:-1], self.cleanValues[1:])]
-        if all(x == diffs[0] for x in diffs) and self.cleanValues[0] == 1:
+        if all(x == diffs[0] for x in diffs) and self.cleanValues[0] == 1 and diffs[0] != 0:
             return True
         return False
 
@@ -104,7 +105,7 @@ class Detection(object):
         """
         diffs = [j-i for i, j in zip(self.cleanValues[:-1], self.cleanValues[1:])]
         mostPopular = max(set(diffs), key=diffs.count)
-        if diffs.count(mostPopular) >= (int(len(diffs)/2)) and mostPopular != 0:
+        if diffs.count(mostPopular) >= (int((len(diffs)+1)/2)) and mostPopular != 0:
             return True
         return False
 
